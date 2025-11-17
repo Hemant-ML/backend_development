5eb2f39b0fcc62a3ce45e0849051467280cb276c
Hemant-ML <hemantsonibalotra@gmail.com>
2025-11-17 23:49:08 +0530

    additon in borrow.

--- rust/borrow/src/main.rs
+++ rust/borrow/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     add_to_string(r2);
     println!("main {}", s);
 }
+fn load(s1: &str){
+    println!("Hello");
+    println!("load {}", s1);
+} 
 
 fn print_string(s1: &str) {
     println!("print_string {}", s1);
@@ -14,3 +18,4 @@ fn print_string(s1: &str) {
 fn add_to_string(s1: &mut String) {
     s1.push_str(" is Awesome");
 }
+
